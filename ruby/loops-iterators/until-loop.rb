e2f670b7b40950aef065ee557bd91b61e7a97557
Rick Flores (nanotechz9l) <0xnanoquetz9l@gmail.com>
2013-10-22 15:15:47 -0700

    infinite-until-loop

---
+++ ruby/loops-iterators/until-loop.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+=begin
+The 'Until' Loop
+The complement to the while loop is the until loop. It's sort of like a backwards while: 
+It will execute its code while the condition it checks is false. 
+As soon as that condition becomes true, the loop will end.
+=end
+
+counter = 1
+until counter == 10
+  puts "counter"
+  # Add code to update 'counter' here!
+  
+  
+end
