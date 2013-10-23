dbde92c62ac93383859fb7dd130917dabb3d1a1b
Rick Flores (nanotechz9l) <0xnanoquetz9l@gmail.com>
2013-10-23 01:19:53 -0700

    added for-loop.rb

---
+++ ruby/loops-iterators/for-loop.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+=begin
+Sometimes you do know how many times you'll be looping, 
+however, and when that's the case, you'll want to use a for loop.
+=end
+
+for num in 1...10 # will not include the # 10!
+  puts num
+end
