3ddcb1365223cd54dc50892c054e7fd70af6cbaa
root <0xnanoquetz9l@gmail.com>
2014-03-13 04:54:19 -0700

    add updated dox

---
+++ ruby/4. Arrays and Hashes/8.Create a Histogram/b.input.rb
@@ -0,0 +1,12 @@
+=begin
+
+You Know the Drill
+
+You know by now how we've gotta start: we need to get input from the user.
+Instructions
+
+Use a puts statement to prompt the user for input. Use gets.chomp to save this input to a variable called text.
+=end
+
+puts "Please enter some misc text"
+text = gets.chomp
