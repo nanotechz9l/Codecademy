3b02b46eb807fe67a50ff86354ba9c152b504eeb
unknown <0xnanoquetz9l@gmail.com>
2014-03-19 18:13:15 -0700

    New changes

--- ruby/4. Arrays and Hashes/7. Data Structures/l.iterating.over.multidim.arrays.rb
+++ ruby/4. Arrays and Hashes/7. Data Structures/l.iterating.over.multidim.arrays.rb
@@ -3,6 +3,7 @@
 Iterating Over Multidimensional Arrays
 
 Iterating over a multidimensional array is slightly trickier.
+=end
 
 things = [[1,2,3], ["red", "blue"]]
 
@@ -14,6 +15,7 @@ things.each do |sub_array|
   end
 end
 
+=begin
     In the above example, we first create an array of arrays.
     Then we print out 2. The array containing numbers is at index zero in things and 2 is at index one in that sub-array.
     Then, we iterate through the things array and use the variable sub_array for the array containing numbers and the array containing colors.
@@ -37,3 +39,12 @@ Puts out every element inside the sub-arrays inside s.
 
 =end
 
+s = [["ham", "swiss"], ["turkey", "cheddar"], ["roast beef", "gruyere"]]
+
+puts s[0][1]
+
+s.each do |sub_array|
+  sub_array.each do |item|
+    puts item
+  end
+end
