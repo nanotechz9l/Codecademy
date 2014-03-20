6370179ef0592b7023dc0eb82c3f1091e60c038f
root <0xnanoquetz9l@gmail.com>
2014-03-20 05:01:06 -0700

    add updated dox

---
+++ ruby/4. Arrays and Hashes/8.Create a Histogram/c.building.words.array.rb
@@ -0,0 +1,16 @@
+=begin
+
+Building the Words Array
+
+Next, we'll want to turn the user's string into something we can iterate over. A data structure made up of elements all in a line, you say? That sounds like an array!
+
+By calling the .split method on text, we can transform it into an array.
+Instructions
+
+Declare a variable called words and set it equal to the result of calling .split on text.
+=end
+
+puts "Please enter some misc text"
+text = gets.chomp
+
+words = text.split
