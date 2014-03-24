6fbc7615bcf909643b93e18cb630f6164993b6d3
unknown <0xnanoquetz9l@gmail.com>
2014-03-24 16:23:23 -0700

    new folder n file

---
+++ ruby/7. Refactoring/13. The Zen of Ruby/a.refactor.intro.rb
@@ -0,0 +1,23 @@
+=begin
+
+Ruby is a Delight
+
+As a language, Ruby prioritizes programmer productivity over program optimization. This means that Ruby may not always run a program in the fastest way possible, but it strives to be a language that programmers (like you!) find easy and fun to use. The more intuitive a language's syntax is, the more productive its users can be. You're in control, not the machine!
+
+Check out the code in the editor. It looks almost just like English, doesn't it?
+Instructions
+
+Click Save & Submit Code when you think you know what the output will be.
+=end
+
+ruby_is_eloquent = true
+ruby_is_ugly = false
+
+puts "Ruby is eloquent!" if ruby_is_eloquent
+puts "Ruby's not ugly!" unless ruby_is_ugly
+
+# Output:
+Ruby is eloquent!
+Ruby's not ugly!
+nil
+
