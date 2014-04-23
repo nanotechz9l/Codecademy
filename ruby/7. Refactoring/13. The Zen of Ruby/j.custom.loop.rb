2f8a7f5227468fb3a46e99bbd91fa587d8cabd5b
nanotechz9l <0xnanoquetz9l@gmail.com>
2014-04-23 13:22:43 -0700

    updated file

--- ruby/7. Refactoring/13. The Zen of Ruby/j.custom.loop.rb
+++ ruby/7. Refactoring/13. The Zen of Ruby/j.custom.loop.rb
@@ -1,20 +1,14 @@
-=begin
+# The Right Tool for the Job
+# Sooner or later, you're going to need to perform a repetitive task in your programs. Many programming languages allow you to do this with a for loop, and while Ruby does include for loops, there are better tools available to us.
 
-The Right Tool for the Job
-
-Sooner or later, you're going to need to perform a repetitive task in your programs. Many programming languages allow you to do this with a for loop, and while Ruby does include for loops, there are better tools available to us.
-
-If we want to do something a specific number of times, we can use the .times method, like so:
+#If we want to do something a specific number of times, we can use the .times method, like so:
 
 5.times { puts "Odelay!" }
 # Prints 5 "Odelay!"s on separate lines
 
-If we want to repeat an action for every element in a collection, we can use .each:
-
+#If we want to repeat an action for every element in a collection, we can use .each:
 [1, 2, 3].each { |x| puts x * 10 }
 # Prints 10, 20, 30 on separate lines
-
-=end
 
 # shortcut array
 my_array = %w{1 2 3 4 5 6 7 8 9 10 11}
@@ -23,14 +17,14 @@ my_array = %w{1 2 3 4 5 6 7 8 9 10 11}
 my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
-#Instructions
+# Instructions
 
-#Let's get a little inventive. Write a loop that only puts the even values of my_array. (Bonus points if you use a one-line if!)
+# Let's get a little inventive. Write a loop that only puts the even values of my_array. (Bonus points if you use a one-line if!)
 
-#Hint
-#Remember, even numbers are evenly divisible by two. You can check this several ways, but we'd probably use this method.
+# Hint
+# Remember, even numbers are evenly divisible by two. You can check this several ways, but we'd probably use this method.
 
-#The exercise wants you to print the even numbers of my_array. Let’s build the code bit by bit, from the inside out:
+# The exercise wants you to print the even numbers of my_array. Let’s build the code bit by bit, from the inside out:
 
 # 1. To print a number x you say
 puts x
