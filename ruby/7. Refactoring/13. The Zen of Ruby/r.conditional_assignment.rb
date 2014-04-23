93bef72b15bd9fdfe5e1ee459830bc5d632e3224
nanotechz9l <0xnanoquetz9l@gmail.com>
2014-04-23 15:41:04 -0700

    add conditional file

---
+++ ruby/7. Refactoring/13. The Zen of Ruby/r.conditional_assignment.rb
@@ -0,0 +1,16 @@
+# Conditional Assignment
+# Perfect! Now let's review conditional assignment. We'll take a break from strict editing mode and let you do a bit more writing.
+
+# Instructions
+# Create a variable called favorite_animal and conditionally assign it to a string containing the name of your favorite animal.
+
+# ?
+# Hint
+# Remember, the conditional assignment operator is ||=.
+
+favorite_animal = nil
+puts favorite_animal
+
+favorite_animal ||= "shark"
+puts favorite_animal
+
