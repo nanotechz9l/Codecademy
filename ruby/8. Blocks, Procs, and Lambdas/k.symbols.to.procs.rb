7ae5a1b6cc4af1148b86edc0bc8f7cf62047df3f
nanotechz9l <0xnanoquetz9l@gmail.com>
2014-05-01 09:04:52 -0700

    updated file

--- ruby/8. Blocks, Procs, and Lambdas/k.symbols.to.procs.rb
+++ ruby/8. Blocks, Procs, and Lambdas/k.symbols.to.procs.rb
@@ -28,3 +28,43 @@ strings_array = numbers_array.map(&:to_s)
 # Output
 ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"] 
 
+
+# Explanation
+to_s
+
+is a method. Each method has a string name, and we're going to convert string to a symbol (unique string) using the
+
+ :
+
+in front of the method name to get
+
+ :to_s
+
+We then take that symbol, and turn it into a proc by adding to the front of it
+
+&
+
+to get
+
+ &:to_s
+
+Now, since we have a proc, we can pass that proc as the argument to either of the methods
+
+.select
+
+or
+
+ .map
+
+These two methods are equivalent. Both take each element in the array one at a time, and apply our proc to them.
+
+.map(&:to_s)
+
+We call this method with this argument to the numbers_array as the receiver object.
+
+numbers_array.map(&:to_s)
+
+Finally, we store the result in a new variable.
+
+strings_array = numbers_array.map(&:to_s)
+
